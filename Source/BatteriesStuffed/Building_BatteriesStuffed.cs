138d3c07c9d021e5a01ff18b654fb87b66cbeff3
Maxim <MaximSw@web.de>
2017-12-30 16:12:28 +0100

    B18-Fix
    Fixed the GenExplosion missing method error.
    Adapted the def files to B18 standards.

--- Source/BatteriesStuffed/Building_BatteriesStuffed.cs
+++ Source/BatteriesStuffed/Building_BatteriesStuffed.cs
@@ -69,7 +69,7 @@ namespace BatteriesStuffed
                 {
                     IntVec3 randomCell = this.OccupiedRect().RandomCell;
                     float radius = Rand.Range(0.5f, 1f) * 3f;
-                    GenExplosion.DoExplosion(randomCell, base.Map, radius, DamageDefOf.Flame, null, null, null, null, null, 0f, 1, false, null, 0f, 1);
+                    GenExplosion.DoExplosion(randomCell, base.Map, radius, DamageDefOf.Flame, null, -1, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false);
                     base.GetComp<CompPowerBattery>().DrawPower(400f);
                 }
             }
